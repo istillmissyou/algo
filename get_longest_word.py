569d6899d98bc5f41c6b7a63d6a98ecc92428c31
Данил Штунь <danilshtun@yandex.ru>
2022-08-09 12:30:48 +0600

    add get_longest_word

---
+++ get_longest_word.py
@@ -0,0 +1,13 @@
+def get_longest_word(line: str) -> str:
+    return max(line.split(), key=len)
+
+def read_input() -> str:
+    _ = input()
+    line = input().strip()
+    return line
+
+def print_result(result: str) -> None:
+    print(result)
+    print(len(result))
+
+print_result(get_longest_word(read_input()))
