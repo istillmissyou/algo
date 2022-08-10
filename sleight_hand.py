fe25a1230e19f43bd7eb30a16db9f1d7475363f4
Данил Штунь <danilshtun@yandex.ru>
2022-08-10 21:14:29 +0600

    fix

--- sleight_hand.py
+++ sleight_hand.py
@@ -4,21 +4,25 @@ NUMBER_OF_LINES = 4
 PLAYERS = 2
 
 
-def sleight_of_hand(clicks: int, char_list: List[str]) -> int:
-    del_char = set()
-    char_dict = {x: 0 for x in set(char_list) if x != '.'}
-    for s in char_list:
-        if s != '.' and s not in del_char:
-            char_dict[s] += 1
-            if char_dict[s] > clicks * PLAYERS:
-                del_char.add(s)
-                del char_dict[s]
-    return len(char_dict)
+def sleight_of_hand(clicks: int, symbols: List[str]) -> int:
+    del_symbols = set()
+    symbol_keys = {x: 0 for x in set(symbols) if x != '.'}
+    players_clicks = clicks * PLAYERS
+    for symbol in symbols:
+        if symbol != '.' and symbol not in del_symbols:
+            symbol_keys[symbol] += 1
+            if symbol_keys[symbol] > players_clicks:
+                del_symbols.add(symbol)
+                del symbol_keys[symbol]
+    return len(symbol_keys)
+
 
 def read_input() -> Tuple[List[str], int]:
     clicks = int(input())
-    char_list = ''.join((input().strip() for _ in range(NUMBER_OF_LINES)))
-    return clicks, list(char_list)
+    symbols = ''.join((input().strip() for _ in range(NUMBER_OF_LINES)))
+    return clicks, list(symbols)
 
-clicks, char_list = read_input()
-print(sleight_of_hand(clicks, char_list))
+
+if __name__ == '__main__':
+    clicks, symbols = read_input()
+    print(sleight_of_hand(clicks, symbols))
