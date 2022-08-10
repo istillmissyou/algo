d4907a13b9f4b804a4152dac266167734c8a9f16
Данил Штунь <danilshtun@yandex.ru>
2022-08-10 20:15:42 +0600

    fix

--- nearest_zero.py
+++ nearest_zero.py
@@ -1,27 +1,31 @@
 from typing import List
 
 
-def nearest_zero(number_list: List[int]) -> List[int]:
-    house_number = len(number_list)
+def nearest_zero(numbers: List[int]) -> List[int]:
+    house_number = len(numbers)
     result = [0] * house_number
-    zero_indices = [i for i in range(house_number) if number_list[i] == 0]
-    for i in range(zero_indices[0], house_number):
-        if number_list[i] == 0:
+    zero_indexes = [i for i, v in enumerate(numbers) if numbers[i] == 0]
+    for i in range(zero_indexes[0], house_number):
+        if numbers[i] == 0:
             result[i] = 0
         else:
             result[i] = result[i - 1] + 1
-    for i in range(zero_indices[-1], zero_indices[0], -1):
-        if number_list[i] == 0:
+    for i in range(zero_indexes[-1], zero_indexes[0], -1):
+        if numbers[i] == 0:
             result[i] = 0
         else:
             result[i] = min(result[i], result[i + 1] + 1)
-    for i in range(zero_indices[0] - 1, -1, -1):
+    for i in range(zero_indexes[0] - 1, -1, -1):
         result[i] = result[i + 1] + 1
     return result
 
-def read_input() -> List[int]:
-    n = int(input())
-    return list(map(int, input().strip().split()))
 
-number_list = read_input()
-print(" ".join(map(str, nearest_zero(number_list))))
+def read_input() -> List[int]:
+    street_length = int(input())
+    house_numbers = list(map(int, input().strip().split()))
+    return house_numbers
+
+
+if __name__ == '__main__':
+    numbers = read_input()
+    print(" ".join(map(str, nearest_zero(numbers))))
