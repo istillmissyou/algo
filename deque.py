81e52aa253fc220d3eb7b05144e1c3b486c1167d
Данил Штунь <danilshtun@yandex.ru>
2022-08-23 12:34:34 +0600

    fix deque

--- deque.py
+++ deque.py
@@ -1,73 +1,80 @@
+class OverflowError(Exception):
+    def __init__(self) -> None:
+        self.message = 'Deque is overflow'
+        super().__init__(self.message)
+
+
+class EmptyError(Exception):
+    def __init__(self) -> None:
+        self.message = 'Deque is empty'
+        super().__init__(self.message)
+
+
 class Deque:
     def __init__(self, max_size: int) -> None:
-        self.queue: list = [None] * max_size
-        self.max_n: int = max_size
-        self.head: int = 0
-        self.tail: int = 0
-        self.size: int = 0
+        self.__queue = [None] * max_size
+        self.__max_n: int = max_size
+        self.__head: int = 0
+        self.__tail: int = 0
+        self.__size: int = 0
 
     def is_empty(self) -> bool:
-        return self.size == 0
+        return self.__size == 0
 
     def is_overflowed(self) -> bool:
-        return self.size == self.max_n
+        return self.__size == self.__max_n
 
     def push_back(self, value: int) -> None:
         if self.is_overflowed():
-            raise IndexError('Deque is overflow')
-        self.queue[self.tail] = value
-        self.tail = (self.tail + 1) % self.max_n
-        self.size += 1
+            raise OverflowError()
+        self.__queue[self.__tail] = value
+        self.__tail = (self.__tail + 1) % self.__max_n
+        self.__size += 1
 
     def push_front(self, value: int) -> None:
         if self.is_overflowed():
-            raise IndexError('Deque is overflow')
-        self.queue[self.head - 1] = value
-        self.head = (self.head - 1) % self.max_n
-        self.size += 1
+            raise OverflowError()
+        head_front = self.__head - 1
+        self.__queue[head_front] = value
+        self.__head = (head_front) % self.__max_n
+        self.__size += 1
 
     def pop_front(self) -> int:
         if self.is_empty():
-            raise IndexError('Deque is empty')
-        value = self.queue[self.head]
-        self.head = (self.head + 1) % self.max_n
-        self.size -= 1
+            raise EmptyError()
+        value = self.__queue[self.__head]
+        self.__head = (self.__head + 1) % self.__max_n
+        self.__size -= 1
         return value
 
     def pop_back(self) -> int:
         if self.is_empty():
-            raise IndexError('Deque is empty')
-        value = self.queue[self.tail - 1]
-        self.tail = (self.tail - 1) % self.max_n
-        self.size -= 1
+            raise EmptyError()
+        tail_back = self.__tail - 1
+        value = self.__queue[tail_back]
+        self.__tail = (tail_back) % self.__max_n
+        self.__size -= 1
         return value
 
 
-def main() -> None:
+if __name__ == '__main__':
     number_commands = int(input())
     max_size = int(input())
     deque = Deque(max_size=max_size)
-
-    commands = {
-        'push_back': deque.push_back,
-        'push_front': deque.push_front,
-        'pop_front': deque.pop_front,
-        'pop_back': deque.pop_back
-    }
 
     for _ in range(number_commands):
         command = input().split()
         if len(command) > 1:
             try:
-                commands[command[0]](value=command[1])
-            except IndexError:
+                getattr(deque, command[0])(value=command[1])
+            except OverflowError:
+                print('error')
+            except EmptyError:
                 print('error')
         else:
             try:
-                print(commands[command[0]]())
-            except IndexError:
+                print(getattr(deque, command[0])())
+            except OverflowError:
                 print('error')
-
-
-if __name__ == '__main__':
-    main()
+            except EmptyError:
+                print('error')
